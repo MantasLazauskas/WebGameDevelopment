fb6d60bc35d16978031cdfe66a6aee300cf227ac
Mantas Lazauskas <mantas.lazauskas@mail.bcu.ac.uk>
2025-11-07 15:39:07 +0000

    Deleted Duplicate Project

--- Web Game Development/eventhandler.js
+++
@@ -1,6 +0,0 @@
-const clickFunction=()=>
-    {
-        document.getElementById("demo").innerHTML = "This is new content";      //Declare the clickFunction methods
-    }
-
-    document.getElementById("demo").addEventListener("click", clickFunction);
